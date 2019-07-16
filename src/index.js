ce5ae3df3b93958cdd75cb90ec3946818f9e25d0
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-07-16 10:52:42 +0200

    Adding dependencies, configuring firebase and updating readme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase')
+require('firebase/firestore')
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDwkA5jyLKscB1hZasPzct6EsWsi3pcQ-4",
+  authDomain: "noteapp-d1a4b.firebaseapp.com",
+  databaseURL: "https://noteapp-d1a4b.firebaseio.com",
+  projectId: "noteapp-d1a4b",
+  storageBucket: "noteapp-d1a4b.appspot.com",
+  messagingSenderId: "304585345640",
+  appId: "1:304585345640:web:71188cf4f0165ed9"
+})f
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
